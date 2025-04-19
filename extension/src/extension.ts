6cf1b87c38fb0dcda0555604d771ad04b855d82e
Stijn <risseeuw.stijn@gmail.com>
2025-04-19 11:42:48 +0200

    Made send button visible in light mode

--- extension/src/extension.ts
+++ extension/src/extension.ts
@@ -306,7 +306,7 @@ class ChatViewProvider implements vscode.WebviewViewProvider {
                 <div id="messages"></div>
                 <div id="input-container">
                     <textarea id="message-input" placeholder="Type a message..."></textarea>
-                    <button id="send-button"><svg class="" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="SendIcon"><path d="M2.01 21 23 12 2.01 3 2 10l15 2-15 2z" fill="white"></path></svg></button>
+                    <button id="send-button"><svg class="" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="SendIcon"><path d="M2.01 21 23 12 2.01 3 2 10l15 2-15 2z" fill="#777778"></path></svg></button>
                 </div>
             </div>
 
